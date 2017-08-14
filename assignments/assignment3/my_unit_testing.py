8f718b606bb97aa6935e0f46f0ca918e9d3f8410
Simon Funke <simon.funke@gmail.com>
2017-08-14 15:01:32 +0200

    Remove assignments

--- assignments/assignment3/my_unit_testing.py
+++
@@ -1,8 +0,0 @@
-class UnitTest(object):
-    def __init__(self, func, args, kwargs, res):    # make test
-        NotImplemented          
-
-    def __call__(self):                             # run test
-        NotImplemented
-
-
--- assignments/assignment5/hello.ny
+++
@@ -1,5 +0,0 @@
-NNNN Naython is a very simple programming language.
-NNNN The only statement is the print statement.
-NNNN Comments are started with four Ns.
-
-print(hello)          NNNN prints hello
