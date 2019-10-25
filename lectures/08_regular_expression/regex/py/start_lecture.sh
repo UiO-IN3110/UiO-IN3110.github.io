9a2279deb7bc960c6c3f0eb055a7e7c86f767796
Simon Funke <simon.funke@gmail.com>
2019-10-25 09:48:34 +0200

    Add a script to open all relevant files

---
+++ lectures/08_regular_expression/regex/py/start_lecture.sh
@@ -0,0 +1,14 @@
+nvim -p  ../rst/01_frontpage.rst \
+../rst/02_editors.rst \
+01_stencil.py \
+02_substitute.py \
+03_challenge.py \
+../rst/03_cancels.rst \
+04_validate_mac.py
+../rst/05_anchors.rst \
+05_cavets.py \
+06_validate_ip.py \
+../rst/06_quantifiers.rst \
+../rst/07_grouping1.rst \
+../rst/08_grouping2.rst \
+../rst/09_grouping3.rst 
