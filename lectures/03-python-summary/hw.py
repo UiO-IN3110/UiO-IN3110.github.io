c9ed7b1e4a62eca23dc975bea0c5188947eaa9a6
Min RK <benjaminrk@gmail.com>
2020-09-02 10:47:06 +0200

    placeholders for hw.py, point

---
+++ lectures/03-python-summary/hw.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from math import sin
+import sys
+
+x = float(sys.argv[1])
+sin_x = sin(x)
+
+print(f"Hello, sin({x:g}) = {sin_x:.3f}")
---
+++ lectures/03-python-summary/point.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+
+class Point:
+    """Class representing a 2-D Cartesian point"""
+
+    def __init__(self, x, y):
+        ...
+
+    def __repr__(self):
+        return f"Point({self.x}, {self.y})"
+
+    def __add__(self, other):
+        """Add two points"""
+        ...
+
+    # subtraction
+    # what's the subtraction magic method?
+    def __whats_subtraction__(self, other):
+        """Subtract one point from another"""
+        ...
+
+    def __mul__(self, other):
+        """Multiply by scalar"""
+        ...
+
+    # matrix multiplication
+    def __whats_matrix_multiply__(self, other):
+        """Dot product of two points, return a scalar"""
+
+
+a = Point(1, 2)
+b = Point(-1, 5)
+print(f"a = {a}")
+print(f"b = {b}")
+print(f"a + b = {a + b}")
+print(f"a - b = {a - b}")
+print(f"a * 5 = {a * 5}")
+print(f"a · b = {a @ b}")
