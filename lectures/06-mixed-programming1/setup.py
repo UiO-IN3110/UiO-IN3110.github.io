6cbafe704340bd1f60c194c28a55692e77943c3c
Simon Funke <simon.funke@gmail.com>
2020-10-17 10:47:04 +0200

    Add web scraping notebook

--- lectures/06-mixed-programming1/setup.py
+++ lectures/06-mixed-programming1/setup.py
@@ -1,5 +1,7 @@
+import numpy
 from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(name='Cython modules',
-      ext_modules=cythonize("*.pyx"))
+      ext_modules=cythonize("*.pyx"),
+      include_dirs=[numpy.get_include()])
