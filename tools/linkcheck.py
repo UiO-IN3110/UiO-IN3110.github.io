eb77529e1c212ed74d35a625bf5b710ddc2a63ca
Min RK <benjaminrk@gmail.com>
2021-09-01 16:24:23 +0200

    add convenient course links to index page

--- tools/linkcheck.py
+++ tools/linkcheck.py
@@ -1,3 +1,4 @@
+import sys
 from bs4 import BeautifulSoup
 import requests
 import requests_cache
@@ -7,19 +8,25 @@ requests_cache.install_cache()
 
 def check_link(url):
     r = requests.get(url)
-    print(r.status_code, url)
+    if r.status_code == 200:
+        sys.stdout.write(".")
+        sys.stdout.flush()
+    else:
+        print(r.status_code, url)
 
 
 def check_links(*paths):
     for path in paths:
-        print(f"Checking {path}")
+        print(f"Checking {path}", end="")
+        count = 0
         with open(path) as f:
             page = BeautifulSoup(f.read(), "html5lib")
             for a in page.find_all("a"):
                 url = a.get("href")
-                if url:
+                if url and not url.startswith("mailto:"):
                     check_link(url)
+        print()
 
 
 if __name__ == "__main__":
-    check_links("index.html")
+    check_links(*(sys.argv[1:] or ["index.html"]))
