817954d1abd36b217f4e4fa5084958a25fe680a2
Min RK <benjaminrk@gmail.com>
2021-11-08 23:32:07 +0100

    altair demo has checkboxes

--- lectures/09_web_programming/altair_web_demo/map_server.py
+++ lectures/09_web_programming/altair_web_demo/map_server.py
@@ -1,9 +1,8 @@
-import json
-import tempfile
+from typing import Optional
 
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from plots import norway_plot
+from plots import get_fylker, plot_daily_cases
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -15,19 +14,19 @@ def norway_plot_html(request: Request):
         "norway_plot.html",
         {
             "request": request,
+            "fylker": get_fylker(),
         },
     )
 
 
 @app.get("/norway_plot.json")
-def norway_plot_json():
-    fig = norway_plot()
-
-    tmp = tempfile.NamedTemporaryFile(suffix=".json")
-    fig.save(tmp.name)
-
-    with open(tmp.name) as file:
-        return json.load(file)
+def norway_plot_json(fylker: Optional[str] = None):
+    # fylker is a query param, a comma-separated list
+    if fylker:
+        fylker = fylker.split(",")
+    # altair Chart.to_dict() is a JSONable vega-lite structure
+    fig = plot_daily_cases(fylker)
+    return fig.to_dict()
 
 
 if __name__ == "__main__":
--- lectures/09_web_programming/altair_web_demo/plots.py
+++ lectures/09_web_programming/altair_web_demo/plots.py
@@ -1,85 +1,92 @@
+import io
+from functools import lru_cache
+
 import altair as alt
 import pandas as pd
+import requests
+
+data_url = "https://raw.githubusercontent.com/thohan88/covid19-nor-data/HEAD/data"
 
 
-def covids_numbers():
-    """ Returns the current covids infections numbers by fylke as a Pandas dictionary """
-
-    data = {
-        "Category": [
-            "Oslo",
-            "Viken",
-            "Vestland",
-            "Rogaland",
-            "Innlandet",
-            "Trøndelag",
-            "Agder",
-            "Vestfold og Telemark",
-            "Troms og Finnmark",
-            "Møre og Romsdal",
-            "Nordland",
-        ],
-        "Insidens": [
-            870.5,
-            410.1,
-            413.8,
-            204,
-            263.3,
-            182.4,
-            201.5,
-            142.1,
-            204.7,
-            151.2,
-            119,
-        ],
-    }
-
-    return pd.DataFrame.from_dict(data)
+def download_dataset(
+    path,
+    parse_dates=[
+        "date",
+    ],
+    data_url=data_url,
+):
+    """Download a dataset from covid19-nor-data archive"""
+    url = f"{data_url}/{path}"
+    print(f"Downloading {url}")
+    r = requests.get(f"{data_url}/{path}")
+    r.raise_for_status()
+    print(f"Downloaded {len(r.content) // 1024}kB")
+    return pd.read_csv(io.BytesIO(r.content), parse_dates=parse_dates)
 
 
-def norway_plot():
-    # Get covids numbers
-    df = covids_numbers()
-
-    # Gets the topojson of norway counties from random github
-    counties = alt.topo_feature(
-        "https://raw.githubusercontent.com/deldersveld/topojson/master/countries/norway/norway-new-counties.json",
-        "Fylker",
+@lru_cache()
+def fylke_data():
+    # get raw case data
+    cases = all_cases = download_dataset(
+        "01_infected/msis/municipality_and_district.csv"
     )
 
-    # Define nearest selection (used for the highlighting)
-    nearest = alt.selection(
-        type="single", on="mouseover", fields=["properties.navn"], empty="none"
+    # aggregate data by fylke
+    cases = (
+        all_cases.groupby(["fylke_name", "date"])[["cases", "population"]]
+        .sum()
+        .reset_index()
     )
+    # discard ukjent fylke where per 100k doesn't make sense
+    cases = cases[~cases.fylke_name.str.contains("Ukjent")]
 
-    # Plot the map
-    fig = (
-        alt.Chart(counties)
-        .mark_geoshape()
+    # 'cases' is a cumulative sum
+    # reverse that to calculate the daily new case count
+    cases["new cases"] = 0.0
+
+    for fylke in cases.fylke_name.unique():
+        mask = cases.fylke_name == fylke
+        fylke_cases = cases.loc[mask]
+        cases.loc[fylke_cases.index, "new cases"] = fylke_cases.cases.diff()
+
+    # per100k is "daily new cases per 100k population"
+    cases["per100k"] = cases["new cases"] * 1e5 / (cases["population"] + 1).astype(int)
+
+    # limit data to 2021
+    return cases[cases.date.dt.year >= 2021]
+
+
+def get_fylker():
+    """Return unique fylke names"""
+    return fylke_data().fylke_name.unique()
+
+
+def plot_daily_cases(fylker=None):
+    # get data
+    cases = fylke_data()
+    # if fylker specified, filter data
+    if fylker:
+        # fylker specified, only display those
+        cases = cases[cases.fylke_name.isin(fylker)]
+
+    # return altair Chart
+    return (
+        alt.Chart(cases)
+        .mark_line()
         .encode(
-            # Enable hover efect
-            tooltip=[
-                alt.Tooltip("properties.navn:N", title="County"),
-                alt.Tooltip("Insidens:Q", title="Cases per 100k capita"),
-            ],
-            color=alt.Color(
-                "Insidens:Q",
-                scale=alt.Scale(scheme="reds"),
-                legend=alt.Legend(title="Cases per 100k capita"),
+            x="date",
+            y=alt.Y(
+                "per100k",
+                scale=alt.Scale(domain=(0, 100)),
             ),
-            stroke=alt.condition(nearest, alt.value("gray"), alt.value(None)),
-            opacity=alt.condition(nearest, alt.value(1), alt.value(0.8)),
-            # Lookup number of cases from Pandas table and map to counties
+            color="fylke_name",
+            tooltip=[
+                "date",
+                "fylke_name",
+                "per100k",
+                "cases",
+                "population",
+            ],
         )
-        .transform_lookup(
-            lookup="properties.navn", from_=alt.LookupData(df, "Category", ["Insidens"])
-        )
-        .properties(
-            width=500,
-            height=600,
-            title="Number of cases per 100k in every county",
-        )
-        .add_selection(nearest)
+        .interactive()
     )
-
-    return fig
