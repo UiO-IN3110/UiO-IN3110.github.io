e4977990fc7ab43c9b2c3a1bf9ac8e2bd1d3e081
Min RK <benjaminrk@gmail.com>
2022-10-12 14:46:42 +0200

    update html, web scraping lecture

--- lectures/07-web-programming/hello_world.py
+++ lectures/07-web-programming/hello_world.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def hello():
     return "Hello World!"
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
