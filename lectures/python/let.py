4b30610ab676be003d579227bf2f38cb75015d21
mirok <miroslav.kuchta@gmail.com>
2023-09-06 15:58:12 +0200

    final lecture version AdvancedPython

---
+++ lectures/python/let.py
@@ -0,0 +1,30 @@
+# Local scopes mimicking let ... end in Juli
+# Inspired by https://stackoverflow.com/questions/61371134/python-let-with-local-scopes-debug-printing-and-temporary-variables
+from contextlib import contextmanager
+from inspect import currentframe, getouterframes
+
+@contextmanager
+def let(**bindings):
+    # 2 because first frame in `contextmanager` is the decorator      
+    frame = getouterframes(currentframe(), 2)[-1][0] 
+    locals_ = frame.f_locals
+    original = {var: locals_.get(var) for var in bindings}
+    locals_.update(bindings)
+    yield
+    locals_.update(original)
+    # Cleanup
+    for var in set(bindings).difference(locals_):
+        del locals_[var]
+        
+# --------------------------------------------------------------------
+if __name__ == '__main__':
+    import importlib
+    
+    b = 3
+    with let(np=importlib.import_module('numpy'), x=2, y=4, b=5):
+        ans = np.zeros((x, y))
+
+    print(ans)
+    print(b)
+    print(np, x, y)
+    
